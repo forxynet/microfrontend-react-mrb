02b54d745f081a6109db1187ab17b59118174729
Hakan Baştürk <forxynet@gmail.com>
2023-12-11 13:14:55 +0300

    complated project

---
+++ marketing/config/webpack.prod.js
@@ -0,0 +1,25 @@
+const { merge } = require('webpack-merge');
+const ModuleFedarationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
+const commonConfig = require('./webpack.common');
+
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+        publicPath: '/marketing/latest/',
+    },
+    plugins: [
+        new ModuleFedarationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './MarketingApp': './src/bootstrap'
+            },
+            shared: packageJson.dependencies
+        })
+    ]
+};
+
+module.exports = merge(commonConfig, prodConfig);
